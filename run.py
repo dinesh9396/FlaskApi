1b49d17af7e1a7b825f360ea018939362df86827
Dinesh <dinesh.melapundi@gmail.com>
2023-07-24 20:22:34 +0530

    modified run.py

--- run.py
+++ run.py
@@ -1,24 +1,4 @@
-from flask import Flask, jsonify, request
-
-
-app = Flask(__name__)
-
-
-todos = []
-
-@app.route('/api/todos', methods=['GET'])
-def get_todos():
-    return jsonify({'todos': todos})
-
-@app.route('/api/todos', methods=['POST'])
-def create_todo():
-    todo = request.get_json().get('todo')
-    if todo:
-        new_todo = {'id': len(todos) + 1, 'title': todo}
-        todos.append(new_todo)
-        return jsonify({'todo': new_todo}), 201
-    else:
-        return jsonify({'error': 'Invalid request'}), 400
+from flaskAPI import app
 
 if __name__ == '__main__':
     app.run(debug=True)
